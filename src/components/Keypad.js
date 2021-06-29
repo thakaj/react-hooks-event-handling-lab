bff79c7f2f85ab9bd2bc74cbcef7f0c0abb0e9a0
thakaj <taulant.hakaj@scranton.edu>
2021-06-29 13:04:06 -0400

    commit tree 385121fb1daafc9521f2998845671f5d25d1e91d

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+
+import React from "react"
+
+function Keypad(){
+    function passwordCallBack(event){
+        return console.log('Entering password...')
+    }
+    return (
+        <input type="password" onChange={passwordCallBack}>
+        
+        </input>
+    )
+}
+
+export default Keypad
